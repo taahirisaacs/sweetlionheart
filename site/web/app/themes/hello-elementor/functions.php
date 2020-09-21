e48e5dfb7d5b4b6bc427a855c3f805e988a65e33
Taahir <taahir.design@gmail.com>
2020-09-21 08:22:23 +0200

    Added FB Pixel to functions.php

--- site/web/app/themes/hello-elementor/functions.php
+++ site/web/app/themes/hello-elementor/functions.php
@@ -197,5 +197,19 @@ function cw_btntext_cart() {
 add_action( 'wp_head', function(){
     ?>
     <meta name="p:domain_verify" content="6e15907dd45d109643f4bbbd47e901b1"/>
-    <?php
+    <!-- Facebook Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+	n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+	document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+	fbq('init', '267776287170318');
+	fbq('track', 'PageView');</script>
+	<noscript><img height='1' width='1' style='display:none'
+	src='https://www.facebook.com/tr?id=267776287170318/&ev=PageView&noscript=1'
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+	<?php
 });
